e5fde40095d5899485d3913978149277d401080f
tamer mohamed <temotimour@gmail.com>
2021-01-02 17:25:35 +0300

    add score.py

--- score.py
+++ score.py
@@ -1,8 +1,10 @@
 import pickle
 import json
 import numpy as np
-from sklearn.externals import joblib
+import joblib
+import pandas
 from azureml.core.model import Model
+
 
 def init():
     global model
@@ -10,9 +12,12 @@ def init():
     model_path = Model.get_model_path('capstone_hyperdrive_best_model')
     model = joblib.load(model_path)
 
-def run(raw_data):
 
-    data = np.array(json.loads(raw_data)['data'])
+def run(raw_data):
+    data = json.loads(raw_data)['data']
+
+    input_data = pandas.DataFrame.from_dict(data)
     # make prediction
-    y_hat = model.predict(data)
+
+    y_hat = model.predict(input_data)
     return json.dumps(y_hat.tolist())
